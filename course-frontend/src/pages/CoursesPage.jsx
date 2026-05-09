553136618298fab5c23be5adbb19339022f6b38f
Joaquin Antonio <jadiazumali@gmail.com>
2026-05-09 13:03:11 +0800

    Incomplete  CoursePage

--- course-frontend/src/pages/CoursesPage.jsx
+++ course-frontend/src/pages/CoursesPage.jsx
@@ -1,8 +1,60 @@
+import { useEffect } from "react";
+import { getAllCourses } from "../services/apiService";
+
 function CoursesPage(){
+    const [courses, setCourses] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        async function loadCourses() {
+            try {
+                const data = await getAllCourses();
+                setCourses(data);
+            } catch (error) {
+                setError("Unable to load courses.");
+                setLoading(false);
+            }
+        }
+        loadCourses();
+    }, []);
     return ( 
         <section>
-            <h1>Courses Page</h1>
-            <p>This page will display courses from the database</p>
+            <div className="page-header">
+                <div>
+                    <h1>Courses</h1>
+                    <p>These courses are loaded from Spring Boot and MongoDB</p>
+                </div>
+            </div>
+
+            {courses.length === 0 ? (
+                <p>No courses found...</p>
+            ) : (
+                <div className="card-grid">
+                    {courses.map((course) => (
+                        <div className="card" key={course.id}>
+                            <h2>{course.title}</h2>    
+
+                            <p>
+                                <strong>Category:</strong> {course.category}
+                            </p>
+
+                            <p>
+                                <strong>Duration:</strong> {course.duration} hours
+                            </p>
+
+                            <p>
+                                <strong>Status:</strong>(" ")
+                                {course.published ? "Published" : "Draft"}
+                            </p>
+
+                            <div className="card-actions">
+                                <Link to={`/courses/${course.id}`}>View Details</Link>
+                            </div>
+                        </div>
+                    ))}                   
+                </div>
+            )}
         </section>
     );
 }
