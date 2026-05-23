c807aac4f177b695443121b7488c078986b067cf
Joaquin Antonio <jadiazumali@gmail.com>
2026-05-23 10:22:39 +0800

    Setup for running frontend tests

--- course-frontend/vite.config.js
+++ course-frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { env } from 'node:process'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.js',
+    exclude: [
+      '**/node_modules/**', 
+      '**/dist/**', 
+      '**/e2e/**',
+      '**/test/**',
+    ]
+  }
 })
